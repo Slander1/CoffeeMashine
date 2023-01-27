b667cafbe093cee47d42def7644277dce052721b
VazirZi <vazir.zivar@mail.com>
2023-01-27 21:01:12 +0500

    Файл класса Americano

---
+++ Americano.cs
@@ -0,0 +1,10 @@
+public class Americano : Coffee
+{
+    public Americano()
+    {
+        tasteAmericano = taste;
+        cookingSpeedAmericano = cookingSpeed;
+    }
+    public double tasteAmericano;
+    public double cookingSpeedAmericano;
+}
