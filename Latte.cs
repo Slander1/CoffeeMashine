70b4e52cc15f433da13ae5c81d1f5f11915f3543
VazirZi <vazir.zivar@mail.com>
2023-01-27 21:01:34 +0500

    Файл класса Latte

---
+++ Latte.cs
@@ -0,0 +1,10 @@
+public class Latte : Coffee
+{
+    public Latte()
+    {
+        tasteLatte = taste;
+        cookingSpeedLatte = cookingSpeed;
+    }
+    public double tasteLatte;
+    public double cookingSpeedLatte;
+}
