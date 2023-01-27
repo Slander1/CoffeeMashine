67a6205b5a0b964da14c763793b0f04ccd4acffa
VazirZi <vazir.zivar@mail.com>
2023-01-27 20:59:54 +0500

    Файл основной программы

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Human human = new Human();
+
+            RojkoveMashine rojkoveMashine = new RojkoveMashine();
+            CapsuleMashine capsuleMashine = new CapsuleMashine();
+
+            human.CookCoffee(rojkoveMashine, 0);
+            Console.WriteLine();
+
+            human.CookCoffee(capsuleMashine, 0);
+            Console.WriteLine();
+
+            human.CookCoffee(rojkoveMashine, 1);
+            Console.WriteLine();
+
+            human.CookCoffee(capsuleMashine, 1);
+            Console.WriteLine();
+
+            human.CookCoffee(rojkoveMashine, 2);
+            Console.WriteLine();
+            
+            human.CookCoffee(capsuleMashine, 2);
+
+        }
+    }
+}
